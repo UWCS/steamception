eddd7976bc2d2d4643e3d50e98dcc11c4b732b01
Matthew Cranham <cranman@hotmail.co.uk>
2012-06-26 15:42:34 +0100

    can get list of steam game IDs for each user

--- query.php
+++ query.php
@@ -5,9 +5,19 @@
 		foreach($names as $key=>$value)
 		{
 			echo($key.'=>'.$value);
+			getSteamProfile($value);
 		}
 	}
 
 	function getSteamProfile($name)
 	{
+		$HEAD_URL = 'http://steamcommunity.com/id/';
+		$TAIL_URL = '/games?tab=all&xml=1';
+		$xml = simplexml_load_file($HEAD_URL.$name.$TAIL_URL) or die('Error loading XML data.');
+
+		foreach($xml->games->game as $game)
+		{
+			echo($game->appID);
+		}
+
 	}
